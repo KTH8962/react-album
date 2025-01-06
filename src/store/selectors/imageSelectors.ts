96372fc0ba1cd7a18194b6170048ee718e2df72e
KTH8962 <kth8962@gmail.com>
2025-01-06 18:41:37 +0900

    env설정

--- src/store/selectors/imageSelectors.ts
+++ src/store/selectors/imageSelectors.ts
@@ -4,7 +4,7 @@ import { searchState } from "../atoms/searchState";
 import { pageState } from "../atoms/pageState";
 
 const API_URL = `https://api.unsplash.com/search/photos`;
-const API_KEY = "2-js4qsXMl2FF7ADWwuOFK2zIv17OGVbxZ_feALQ-uU";
+const API_KEY = import.meta.env.VITE_API_KEY;
 const PER_PAGE = 30;
 
 export const imageData = selector({
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,9 @@
 /// <reference types="vite/client" />
+interface ImportMetaEnv {
+  readonly VITE_API_KEY: string;
+  // 필요한 다른 환경 변수를 추가할 수 있습니다.
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
